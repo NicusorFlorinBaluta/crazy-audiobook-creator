6a0bfd00194afdf1b87954e45865e28724821ae8
Nick <nicusor.florin.baluta@gmail.com>
2026-08-10 09:55:56 +0300

    chore: remove obsolete diagnostic scripts

--- read_json.py
+++
@@ -1,6 +0,0 @@
-import json
-with open('e:/Projects/crazy-audiobook-creator/voice_library/sample_book-12/voices.json') as f:
-    data = json.load(f)
-    nm = data['voices'].get('narrator_male', {})
-    print('narrator_male file:', nm.get('file'))
-    print('narrator_male keys:', list(nm.keys()))
